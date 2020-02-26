d5b7f735cad1a6e3d18bd4e87446e979d1ef9b49
Guntars Kirovainis <guntars.kirovainis@gmail.com>
2020-02-27 01:02:26 +0200

    Adding loop to repeat game without terminating program

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,12 +1,23 @@
+import java.util.Scanner;
+
 public class Launcher {
 
     public static void main(String[] args) {
 
-        Game game = new Game();
-        game.printBoardWithPositions();
-        while (!game.hasGameEnded()) {
-            game.nextMove();
-        }
+        Scanner sc = new Scanner(System.in);
+        String playAgain = "";
+
+        do {
+            Game game = new Game();
+            game.printBoardWithPositions();
+            while (!game.hasGameEnded()) {
+                game.nextMove();
+            }
+            System.out.println("Play again? Enter Y to continue or any other key to quit: ");
+            playAgain = sc.nextLine();
+        } while (playAgain.equalsIgnoreCase("y"));
+
+        System.out.println("Thank you for playing!");
 
     }
 }
